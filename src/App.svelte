84c652465f89bf6d13c55e413e457d7052c5b03d
Dominick Bartenope <dominickbartenope@yahoo.com>
2020-05-20 16:56:21 -0400

    clean up

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+  let name = "Dom";
+</script>
+
+<style>
+  h1 {
+    font-size: 4em;
+    font-weight: 100;
+    color: red;
+    text-align: center;
+    text-transform: uppercase;
+  }
+</style>
+
+<h1>Hello {name}.</h1>
